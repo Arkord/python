6f763887b042b703170d71e2fb7a32449418cb9c
arkord <arkord1@gmail.com>
2023-10-02 10:14:44 -0600

    init

---
+++ control.py
@@ -0,0 +1,11 @@
+continuar = True;
+numero = 0;
+
+while continuar:
+    print("loop")
+    numero = int(input("Proporciona un número: "))
+
+    if numero == 1:
+        continuar = False
+
+        
