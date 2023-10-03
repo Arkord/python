b02edadade5eeacefb2ddebe4b1721ef5ddc78af
arkord <arkord1@gmail.com>
2023-10-03 11:28:15 -0600

    listas

--- estructuras1.py
+++ estructuras1.py
@@ -1,9 +1,19 @@
+def imprimir(lista):
+    print("Estas son las frutas que tienes en tu canasta: ")
+    for item in lista:
+       print(item)
+
 lista = ["plátano", "naranja", "manzana", "limón", "guayaba"]
 
-for item in lista:
-    print(item)
+seguir = "SI";
 
-seguir = True;
-
-while seguir:
+while  seguir.upper() == "SI":
     print("Bienvenido a la frutería Don Carlos, selecciona una opción:\n")
+
+    fruta = input("Ingresa una nueva fruta: ")
+    lista.append(fruta)
+
+    imprimir(lista)
+
+    seguir = input("¿Seguir? ").upper()
+
