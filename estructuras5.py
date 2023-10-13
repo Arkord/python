e06cc221101f66152c86bdfdec554de54b8c7040
arkord <arkord1@gmail.com>
2023-10-13 08:17:12 -0600

    pop

--- estructuras5.py
+++ estructuras5.py
@@ -3,9 +3,23 @@ print("Diccionarios")
 diccionario = {}
 diccionario.update({"ajo en polvo": 100})
 diccionario.update({"pimienta molida": 200})
+diccionario.update({
+    "lista": ["item1", "item2", "item3"]
+})
 
-# print(diccionario)
+diccionario["pimienta molida"] = 500
 
-for elemento in diccionario:
-    print(elemento)
-    print(f"Valor: {diccionario[elemento]}")
+# diccionario.popitem()
+# diccionario.items
+# print(diccionario["pimienta molida"])
+# diccionario.get("pimienta molida")
+# diccionario.popitem()
+# diccionario.pop("lista")
+
+print(diccionario.values())
+print(diccionario.keys())
+print(diccionario.items())
+
+# for elemento in diccionario:
+#     print(elemento)
+#     print(f"Valor: {diccionario[elemento]}")
